dfed66d0f7596ed09552ced71684921c72f22579
ArcanePayload <90263927+ArcanePayload@users.noreply.github.com>
2022-02-08 09:18:13 -0600

    ..

--- Castle game/Assets/playermovement.cs
+++ Castle game/Assets/playermovement.cs
@@ -2,27 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    //Componet referneces
+    //Component references
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sr;
 
-    //Movement variables
-    private float horizVelocity;
-    private float vertVelocity;
-    public float fallMulitplier = 2.5f;
-    public float lowJumpMulitplier = 2f;
-
-    //Ground Checking Variables
-    private bool isGrounded;
-    public Transform isGroundedChecker;
-    public float CheckGroundRadius;
-    public LayerMask groundLayer;
-
+    //movement variables
+    private float hVelo;
     public float speed = 4;
-    public float jumpforce = 5.5f;
+    private float vertVelo;
+    
+   
+
+
+    //Ground 
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundlayer;
+    private bool isGrounded;
+
+    //health variables
+    public int health;
 
 
     // Start is called before the first frame update
@@ -37,23 +39,68 @@ public class playermovement : MonoBehaviour
     void Update()
     {
         MoveH();
-        jump();
+       
         CheckIfGrounded();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    void MoveH()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        horizVelocity = x * speed;
-        rb.velocity = new Vector2(horizVelocity, rb.velocity.y);
 
-        anim.SetFloat("Speed", Mathf.Abs(horizVelocity));
+    public void TakeDamage(int damageTaken)
+    {
+        health -= damageTaken;
+    }
+
+
+
+
+
+    void MoveH()
+    {    //making charater move//
+
+        float x = Input.GetAxisRaw("Horizontal");
+        hVelo = x * speed;
+        rb.velocity = new Vector2(hVelo, rb.velocity.y);
+
+        //making animations go burrrrrr//
+
+        anim.SetFloat("Speed", Mathf.Abs(hVelo));
 
         if (x > 0)
         {
             sr.flipX = false;
         }
+
         else if (x < 0)
         {
             sr.flipX = true;
         }
+
+
+
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D gCollider = Physics2D.OverlapCircle(
+            isGroundedChecker.position,
+            checkGroundRadius,
+            groundlayer);
+
+        if (gCollider != null)
+        {
+            isGrounded = true;
+        }
+
+        else
+        {
+            isGrounded = false;
+        }
+        anim.SetBool("Grounded", isGrounded);
+    }
+
+
+}
