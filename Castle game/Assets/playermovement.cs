3120272ad950adb7748c3acbf347e9b358ce6351
justin bellamy <50855196+Jawa-UwU@users.noreply.github.com>
2022-02-10 08:44:32 -0600

    update playermovement

--- Castle game/Assets/playermovement.cs
+++ Castle game/Assets/playermovement.cs
@@ -13,94 +13,10 @@ public class PlayerMovement : MonoBehaviour
     private float hVelo;
     public float speed = 4;
     private float vertVelo;
-    
-   
 
 
-    //Ground 
-    public Transform isGroundedChecker;
-    public float checkGroundRadius;
-    public LayerMask groundlayer;
-    private bool isGrounded;
-
-    //health variables
-    public int health;
-
-
-    // Start is called before the first frame update
-    void Start()
+   void update()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
+        if(Input.GetKey(KeyCode.W))
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveH();
-       
-        CheckIfGrounded();
-
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-
-    public void TakeDamage(int damageTaken)
-    {
-        health -= damageTaken;
-    }
-
-
-
-
-
-    void MoveH()
-    {    //making charater move//
-
-        float x = Input.GetAxisRaw("Horizontal");
-        hVelo = x * speed;
-        rb.velocity = new Vector2(hVelo, rb.velocity.y);
-
-        //making animations go burrrrrr//
-
-        anim.SetFloat("Speed", Mathf.Abs(hVelo));
-
-        if (x > 0)
-        {
-            sr.flipX = false;
-        }
-
-        else if (x < 0)
-        {
-            sr.flipX = true;
-        }
-
-
-
-    }
-
-    void CheckIfGrounded()
-    {
-        Collider2D gCollider = Physics2D.OverlapCircle(
-            isGroundedChecker.position,
-            checkGroundRadius,
-            groundlayer);
-
-        if (gCollider != null)
-        {
-            isGrounded = true;
-        }
-
-        else
-        {
-            isGrounded = false;
-        }
-        anim.SetBool("Grounded", isGrounded);
-    }
-
-
 }
